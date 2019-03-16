9133fa097113272325ebcf8004af57ca2334fae6
Brandon Foltz <bsfoltzguitar@gmail.com>
2019-03-16 14:28:55 -0400

    Moved templates, js and css to appropriate places in the app.

--- src/routes.php
+++ src/routes.php
@@ -4,16 +4,24 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
-
-/*$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+/*
+ * This is the defailt "slim app" route just to use as an example
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
-});*/
+});
+*/
 
+//This is the "home page" route that just loads the app
+$app->get('/', function (Request $request, Response $response, array $args) {
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+//This is the REST style API endpoint that our front-end communicates with.
 $app->get('/translate', function (Request $request, Response $response, array $args) {
-    $args['content'] = "Translate route is not finished.";
+    $args['content'] = "Translate route is not finished. This will send and receive some JSON when done.";
     return $this->renderer->render($response, 'content.phtml', $args);
 });
