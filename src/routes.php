00b13f94308f2e64293bc457d32b803fa02c417e
Brandon Foltz <bsfoltzguitar@gmail.com>
2019-03-19 19:09:29 -0400

    WIP /translate route. Trying to validate incoming JSON requests and return an error if necessary. Included src/classes/ in path loaded by autoloader, so we can put our custom PHP classes there.

--- src/routes.php
+++ src/routes.php
@@ -2,26 +2,35 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
-
-// Routes
-/*
- * This is the defailt "slim app" route just to use as an example
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
-*/
+use ImageTranslator\TranslationRequestValidator;
 
 //This is the "home page" route that just loads the app
 $app->get('/', function (Request $request, Response $response, array $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
+$app->get('/good', function (Request $request, Response $response, $args = []) {
+    return $response->withStatus(200);
+
+});
+
 //This is the REST style API endpoint that our front-end communicates with.
-$app->get('/translate', function (Request $request, Response $response, array $args) {
-    $args['content'] = "Translate route is not finished. This will send and receive some JSON when done.";
-    return $this->renderer->render($response, 'content.phtml', $args);
+$app->post('/translate', function (Request $request, Response $response, array $args) {
+    $translateRequest = $request->getParsedBody();
+
+    //echo "Got here line 21";
+    $translateRequest = TranslationRequestValidator::get_translate_array_errors($translateRequest);
+    //echo "Got here line 22";
+    if (empty($translateRequest)) {
+        return $response->withStatus(400);
+    }
+    print_r($translateRequest);
+    foreach ($translateRequest as $k => $v) {
+        if (array_key_exists('errors', $v)) {
+            return $response->withJson($translateRequest, 200); //just send back the JSON request with error fields filled out.
+        }
+    }
+    echo "Got here line 29";
+
+    return $response->withJson($errorExample, 200);
 });
