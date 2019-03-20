34d953f5707b4960b29731bedc95c5c362cdde26
Brandon Foltz <bsfoltzguitar@gmail.com>
2019-03-19 22:20:33 -0400

    Tidying code and taking notes for demo video

--- src/public/js/web.js
+++ src/public/js/web.js
@@ -1,100 +1,105 @@
 function readURL(input) {
-  if (input.files && input.files[0]) {
+    if (input.files && input.files[0]) {
 
-    var reader = new FileReader();
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('.image-upload-wrap').hide();
+            $('.file-upload-image').attr('src', e.target.result);
+            $('.file-upload-content').show();
+            $('.image-title').html(input.files[0].name);
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        removeUpload();
+    }
 
-    reader.onload = function(e) {
-      $('.image-upload-wrap').hide();
+    //TODO call OCR function
 
-      $('.file-upload-image').attr('src', e.target.result);
-      $('.file-upload-content').show();
+    //show a progress bar for OCR
 
-      $('.image-title').html(input.files[0].name);
 
-    };
-
-    reader.readAsDataURL(input.files[0]);
-
-  } else {
-    removeUpload();
-  }
 }
 
 function removeUpload() {
-  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-  $('.file-upload-content').hide();
-  $('.image-upload-wrap').show();
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
 }
+
 $('.image-upload-wrap').bind('dragover', function () {
-		$('.image-upload-wrap').addClass('image-dropping');
-	});
-	$('.image-upload-wrap').bind('dragleave', function () {
-		$('.image-upload-wrap').removeClass('image-dropping');
+    $('.image-upload-wrap').addClass('image-dropping');
 });
 
-
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
+        
 function performOCR() {
-  /*
-    Placeholder function which calls translateReq()
-  */
-  translateReq([
+    /*
+     Placeholder function which calls translateReq()
+     */
+    translateReq([
     {
-      id: 1,
-      source_language: 'latin',
-      destination_language: 'english',
-      text: 'Bellum est malo'
+        id: 1,
+        source_language: 'latin',
+        destination_language: 'english',
+        text: 'Bellum est malo'
     }
-  ])
-  .then(translatedText => {
-    console.log(translatedText);
-    // Do something with text..
-  })
-  .catch(error => {
-    console.error(error);
-  })
+    ])
+    .then(translatedText => {
+        console.log(translatedText);
+        // Do something with text..
 
+        //TODO render the text back on the Canvas corresponding to where it came from
+        //-> related, should correspond to ID number which is associated with
+        //-> blocks of text detected by tesseract.js
+
+    })
+    .catch(error => {
+        console.error(error);
+    });
 }
 
 
 async function translateReq(textList) {
-  /*
-    @desc
-      Performs an AJAX request to the /translate url using http POST method.
- 
-    @param object textList
-      This is a javascript object that contains a list of objects containing text as well as meta data describing translating preferences. For example:
-      const textList = [
-        {
-          id: 1,
-          source_language: 'latin',
-          destination_language: 'english',
-          text: 'Bellum est malo'
-        }
-      ];
-     
-    @return
-      A javascript object that contains a list of objects containing the translated text as well as meta data describing the translation. This data is received from the server over AJAX. For example:
-      const textList = [
-        {
-          id: 1,
-          source_language: 'latin',
-          destination_language: 'english',
-          translated_text: 'War is bad'
-        }
-      ];
- 
-    @throws Exception on unsuccessful network connection to the server.
-  */
+    /*
+     @desc
+     Performs an AJAX request to the /translate url using http POST method.
 
-  const res = await fetch('../translate.json', { // Dummy url endpoint for now
-    method: 'GET', //Should be POST
-    //body: JSON.stringify(textList),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(response => response.json()).catch(error => {
-    console.error(error);
-  });
-  return res;
+     @param object textList
+     This is a javascript object that contains a list of objects containing text as well as meta data describing translating preferences. For example:
+     const textList = [
+         {
+             id: 1,
+             source_language: 'latin',
+             destination_language: 'english',
+             text: 'Bellum est malo'
+         }
+     ];
+
+     @return
+     A javascript object that contains a list of objects containing the translated text as well as meta data describing the translation. This data is received from the server over AJAX. For example:
+     const textList = [
+         {
+             id: 1,
+             source_language: 'latin',
+             destination_language: 'english',
+             translated_text: 'War is bad'
+         }
+     ];
+
+     @throws Exception on unsuccessful network connection to the server.
+     */
+
+    const res = await fetch('/translate', { // Dummy url endpoint for now
+        method: 'POST', //Should be POST
+        //body: JSON.stringify(textList),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => response.json()).catch(error => {
+        console.error(error);
+    });
+    return res;
 }
  
