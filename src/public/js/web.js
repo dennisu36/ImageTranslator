5746a9304b9c7273dced65422799ef1e96dc34d4
Choekgi Kalsang <kalsang@temple.edu>
2019-03-20 11:49:11 -0400

    fixed

--- src/public/js/web.js
+++ src/public/js/web.js
@@ -1,23 +1,31 @@
+var validTypes = ['jpg', 'jpeg', 'png', 'pdf'];
 function readURL(input) {
   if (input.files && input.files[0]) {
+    var extension = input.files[0].name.split('.').pop().toLowerCase(),
+     isSuccess = validTypes.indexOf(extension) > -1;
+      if (isSuccess) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+            }
 
-    var reader = new FileReader();
-
-    reader.onload = function(e) {
+          reader.onload = function(e) {
       $('.image-upload-wrap').hide();
 
       $('.file-upload-image').attr('src', e.target.result);
       $('.file-upload-content').show();
 
       $('.image-title').html(input.files[0].name);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-
-  } else {
-    removeUpload();
+    }
+       reader.readAsDataURL(input.files[0]);
+      }
+      else{
+          alert('Invalid File Type. Please insert JPG, JPEG, PNG, or PDF files.')
+          removeUpload();
+      }
   }
 }
+
+
 
 function removeUpload() {
   $('.file-upload-input').replaceWith($('.file-upload-input').clone());
@@ -25,8 +33,12 @@ function removeUpload() {
   $('.image-upload-wrap').show();
 }
 $('.image-upload-wrap').bind('dragover', function () {
-		$('.image-upload-wrap').addClass('image-dropping');
-	});
-	$('.image-upload-wrap').bind('dragleave', function () {
-		$('.image-upload-wrap').removeClass('image-dropping');
+                $('.image-upload-wrap').addClass('image-dropping');
+        });
+        $('.image-upload-wrap').bind('dragleave', function () {
+                $('.image-upload-wrap').removeClass('image-dropping');
 });
+~                                                                                                                   
+~                                                                                                                   
+~                                                                                                                   
+~        
